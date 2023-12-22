2d027deaf4f50d45183c790a991a9fda30c50c4f
bartek-panda <bartlomiejzdunek88@gmail.com>
2023-12-22 19:39:14 +0000

    Add files

---
+++ provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket = "bartlomiej-zdunek-panda-devops-core-16"
+    key = "infra/terraform.state"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.33"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
